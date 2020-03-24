61541ddfae0fd967e5f137dbc3310838b72ad60a
Jan Radziminski <radziminski.j@gmail.com>
2020-03-24 21:35:08 +0100

    Updates

---
+++ precache-manifest.5ed39a70c8e97b00a0ce11fae70de4a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035011939261761b4069622afcc3ecc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8051f9467c89deb546a",
+    "url": "/static/css/main.d77d798f.chunk.css"
+  },
+  {
+    "revision": "8fc1efca28197a1945a2",
+    "url": "/static/js/2.db9db827.chunk.js"
+  },
+  {
+    "revision": "b8319ac6acf282e7633739022223eecc",
+    "url": "/static/js/2.db9db827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8051f9467c89deb546a",
+    "url": "/static/js/main.3f41d75c.chunk.js"
+  },
+  {
+    "revision": "4e5f66d1e02008f150ba",
+    "url": "/static/js/runtime-main.7db85eed.js"
+  }
+]);
